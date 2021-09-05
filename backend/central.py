ab606199daf6d6ec764407d81c2d61c100f01bf6
Natthanon Manop <natthanon.manop@gmail.com>
2021-09-05 12:23:36 +0700

    remove get_answer function

--- backend/central.py
+++ backend/central.py
@@ -36,16 +36,3 @@ def init_inhit(building_name: str) -> InHit:
             data.append(json.loads(line))
     inhit = InHit(100, 3, 100, data)
     return inhit
-
-
-def get_answer(
-    exthit_obj: ExtHit_Bldg, inhit_obj: InHit, access_point: List[Dict[str, Any]]
-) -> Dict[str, Any]:
-    building_ans = exthit_obj.localize(access_point)
-    inhit_ans = inhit_obj.localize(access_point)
-    ans_dict = {
-        "building": building_ans[0],
-        "floor": inhit_ans["floor"],
-        "tag": inhit_ans["tag"],
-    }
-    return ans_dict
