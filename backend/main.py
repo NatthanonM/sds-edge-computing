44e70896b29962a157191c8bf6e1f602a8f28ac5
Natthanon Manop <natthanon.manop@gmail.com>
2021-10-30 13:27:39 +0700

    remove unused code

--- backend/main.py
+++ backend/main.py
@@ -52,28 +52,5 @@ def get_location():
     return jsonify(res)
 
 
-@app.route("/<int:id>")
-def hello_world(id):
-    if id < 0 or id > len(example_id):
-        abort(404, description="Not found")
-    start = time.time()
-    input_query = example_query[example_id[id]]
-    ans = (inhit_dict[example_building[id]]).localize(input_query)
-    end = time.time()
-    return f"<p>Hello, Edge!</p><p>{example_building[id]} {ans}</p><p>response time: {(end - start) * 1000} ms</p>"
-
-
-@app.route("/central/<int:id>")
-def hello_central(id):
-    if id < 0 or id > len(example_id):
-        abort(404, description="Not found")
-    start = time.time()
-    central_address = f"http://54.179.136.231:5000/central/{id}"
-    r = requests.get(central_address)
-    building_id, location = r.json()["building_id"], r.json()["location"]
-    end = time.time()
-    return f"<p>Hello, Central!</p><p>{building_id} {location}</p><p>response time: {(end - start) * 1000} ms</p>"
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
